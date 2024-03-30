dbd6d5da0d2c28b4a674d23d3e3a9313d91b72c0
yui666a <haiso666@gmail.com>
2024-03-31 02:16:45 +0900

    feat: tanstack query を使用

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
+
+const fetchHealth = async () => {
+  const response = await axios.get(
+    'https://aiso-reminder-server-cloudrun-obcs3lfy6a-uc.a.run.app/',
+  );
+  return response.data;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+  const { isLoading, error, data } = useQuery({
+    queryKey: ['helloWorld'], // 配列
+    queryFn: fetchHealth, // データ取得の関数
+  });
+
+  // (['repoData'], () =>
+  //   axios.get('https://api.github.com/repos/tannerlinsley/react-query'),
+  // );
+
+  if (isLoading) {
+    return 'loading...';
+  }
+
+  if (error) {
+    return 'error!';
+  }
+
+  console.log(data);
 
   return (
     <>
@@ -29,7 +57,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
