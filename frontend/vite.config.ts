945f7a58d04ec96e0f746ad8f9b47627337b3108
yui666a <haiso666@gmail.com>
2024-03-31 23:33:13 +0900

    check: GCSでデプロイできるようにするため，base pathを追加

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/school-reminder-web-aiso/',
   // https://qiita.com/ginger-yell/items/602091d7010f77fbb47a
   server: {
     host: true,
