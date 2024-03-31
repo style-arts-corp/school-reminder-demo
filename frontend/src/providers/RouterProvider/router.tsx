df6610c98fd4d626e2e7a4de85452b19e18c2549
yui666a <haiso666@gmail.com>
2024-04-01 01:06:47 +0900

    try: createBrowserRouter のオプションにbasenameをつけて，Github pagesが使えるか検証

--- frontend/src/providers/RouterProvider/router.tsx
+++ frontend/src/providers/RouterProvider/router.tsx
@@ -1,18 +1,23 @@
 import IndexPage from '@/pages/index';
 import { createBrowserRouter } from 'react-router-dom';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <IndexPage />,
+      children: [],
+    },
+    {
+      path: 'dashboard',
+      element: <div>dashboard のコンポーネントを作成してください</div>,
+    },
+    {
+      path: 'about',
+      element: <div>about のコンポーネントを作成してください</div>,
+    },
+  ],
   {
-    path: '/',
-    element: <IndexPage />,
-    children: [],
+    basename: 'school-reminder-demo',
   },
-  {
-    path: 'dashboard',
-    element: <div>dashboard のコンポーネントを作成してください</div>,
-  },
-  {
-    path: 'about',
-    element: <div>about のコンポーネントを作成してください</div>,
-  },
-]);
+);
