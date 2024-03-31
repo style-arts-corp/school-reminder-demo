2b561bf440c6a3520089f20579e3b43dfcf39b86
yui666a <haiso666@gmail.com>
2024-03-31 12:57:26 +0900

    feat: front 画面を調整

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,12 +2,20 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useQuery } from '@tanstack/react-query';
 import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+// import viteLogo from '/vite.svg';
+import muiLogo from './assets/mui.svg';
+import reactRouterLogo from './assets/react-router.svg';
+import reactHookFormLogo from './assets/react-hook-form.svg';
 import './App.css';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import { Link } from 'react-router-dom';
+import { Button } from '@mui/material';
 
 const fetchHealth = async () => {
   const response = await axios.get(
-    'https://aiso-reminder-server-cloudrun-obcs3lfy6a-uc.a.run.app/',
+    // 'https://aiso-reminder-server-cloudrun-obcs3lfy6a-uc.a.run.app/',
+    'http://localhost:3001',
   );
   return response.data;
 };
@@ -20,42 +28,64 @@ function App() {
     queryFn: fetchHealth, // データ取得の関数
   });
 
-  // (['repoData'], () =>
-  //   axios.get('https://api.github.com/repos/tannerlinsley/react-query'),
-  // );
-
   if (isLoading) {
-    return 'loading...';
+    // NOTE: 本当は return <Loading /> とかにするべきだが，サーバーから取得したデータを表示していることを強調するために何もしないようにしている
+    console.log('loading...');
   }
 
   if (error) {
     return 'error!';
   }
 
-  console.log(data);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
+      <Box>
+        {/* <Link to="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
+        </Link> */}
+
+        <Link to="https://ja.react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        </Link>
+
+        <Link to="https://mui.com/material-ui/" target="_blank">
+          <img src={muiLogo} className="logo" alt="Mui logo" />
+        </Link>
+
+        <Link to="https://reactrouter.com/en/main" target="_blank">
+          <img src={reactRouterLogo} className="logo" alt="React Router logo" />
+        </Link>
+        <Link to="https://react-hook-form.com/" target="_blank">
+          <img
+            src={reactHookFormLogo}
+            className="logo"
+            alt="React Hook Form logo"
+          />
+        </Link>
+      </Box>
+
+      <h1>React + MUI + React Router + React Hook Form</h1>
+      <Box className="card">
+        <Button
+          variant="contained"
+          onClick={() => setCount((count) => count + 1)}
+        >
           count is {count}
-        </button>
+        </Button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          <code>src/App.tsx</code>{' '}
+          を編集＆ファイル保存してリロードしてみてください。
         </p>
-      </div>
+      </Box>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        React logo や MUI logo
+        をクリックして公式サイトにアクセスしてみてください。
       </p>
+
+      <p className="read-the-docs">サーバーから取得した文字列は...</p>
+      <Typography variant="h1" component="h2">
+        {data?.message || 'loading...'}
+      </Typography>
     </>
   );
 }
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import CircularProgress from '@mui/material/CircularProgress';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -12,10 +14,26 @@ const queryClient = new QueryClient({
   },
 });
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [],
+  },
+  {
+    path: 'dashboard',
+    element: <div>dashboard のコンポーネントを作成してください</div>,
+  },
+  {
+    path: 'about',
+    element: <div>about のコンポーネントを作成してください</div>,
+  },
+]);
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <RouterProvider router={router} fallbackElement={<CircularProgress />} />
     </QueryClientProvider>
   </React.StrictMode>,
 );
